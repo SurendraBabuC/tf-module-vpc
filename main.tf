7c4486c54f3e567c3bb38c18e3e398bdc8ac2b1f
C SURENDRA BABU <surendrababu.c01@gmail.com>
2023-06-03 18:44:37 +0530

    Ansible

--- main.tf
+++ main.tf
@@ -65,8 +65,7 @@ resource "aws_route" "peering_connection_route" {
 }
 
 resource "aws_route" "peering_connection_route_in_default_vpc" {
-  count = length(local.all_private_subnet_ids)
-  route_table_id = element(local.all_private_subnet_ids, count.index)
+  route_table_id = var.default_vpc_rtid
   vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
   destination_cidr_block = var.cidr_block
 }
